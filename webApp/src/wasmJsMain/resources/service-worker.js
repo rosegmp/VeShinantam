ee0ba835a9fde80e1092c8be43c1c29205949189
Aaron Engelman <aaron@destinyhomesnj.com>
2026-09-15 13:18:51 -0400

    Retry transient web sync failures

--- webApp/src/wasmJsMain/resources/service-worker.js
+++ webApp/src/wasmJsMain/resources/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'veshinantam-web-v13';
+const CACHE = 'veshinantam-web-v14';
 const SHELL = ['./', './index.html', './veshinantam.js', './entity-sync.js', './supabase-config.js', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
